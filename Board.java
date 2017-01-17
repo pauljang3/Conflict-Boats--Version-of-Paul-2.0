d8b5880772f767e4737a022e794e4fca691acf49
w1sely <chen.wisley@gmail.com>
2017-01-17 11:10:32 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,39 +1,66 @@
+
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Board {
+public class Divider extends JPanel {
   
-  static boolean isGameOn = true;
-  static boolean hasSetupDonePlayer1 = false;
-  static boolean hasSetupDonePlayer2 = false;
-
- public static void main(String[] args) {
+  JButton[] boatButtons = new JButton[5];
   
-  JFrame frame = new JFrame("Battleship");
-  frame.setSize(1900, 1000);
-  frame.setLayout(new GridLayout(1, 3));
+  public final String[] size = new String[1];
   
-  Divider divider = new Divider();
-  LeftField leftField = new LeftField(divider);
-  
-  frame.add(leftField);
-  frame.add(divider);
-  frame.add(new RightField());
-  frame.setLocationRelativeTo(null);
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  frame.setVisible(true);
-  frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-  
-//  while(isGameOn){
+  public Divider()
+  {
+    init();
+  }
+  private void init() {
+    ImageIcon ac=new ImageIcon (this.getClass().getResource("aircraftcarrier.jpg"));
+    ImageIcon destroyer=new ImageIcon (this.getClass().getResource("destroyer.jpg"));
+    ImageIcon sub=new ImageIcon (this.getClass().getResource("submarine.jpg"));
+    ImageIcon patrol=new ImageIcon (this.getClass().getResource("patrol.jpg"));
     
-
+    setLayout(new BorderLayout());
+    JPanel divider = new JPanel();
+    add(divider, BorderLayout.NORTH);
     
     
-
+    JLabel title = new JLabel("                                                                                                 Ships");
+    add(title, BorderLayout.NORTH);
     
-//    isGameOn = false;
-//  }
-
- }
+    JPanel middlePanel = new JPanel();
+    middlePanel.setLayout(new GridLayout(6 , 1));
+    // ActionListener buttonListen = new ActionListener();
+    for (int i = 4; i >= 0; i--){
+        boatButtons[i] = new JButton();
+        boatButtons[i].setName((i+1)+"");
+        boatButtons[i].setPreferredSize(new Dimension(10,10));                                
+        //  jb[i][j].addActionListener(buttonListen);
+        middlePanel.add(boatButtons[i]);
+        boatButtons[i].addActionListener(new ActionListener(){
+          
+          public void actionPerformed(ActionEvent event) {
+            
+            
+            System.out.println(((JButton)event.getSource()).getName());
+            size[0] = ((JButton)event.getSource()).getName();
+            
+          }
+          
+        });
+      
+    }
+    boatButtons[4].setIcon(ac);
+    boatButtons[3].setIcon(destroyer);
+    boatButtons[2].setIcon(sub);
+    boatButtons[1].setIcon(patrol);
+    
+    add(middlePanel, BorderLayout.CENTER);
+    
+  }
+  
+  public JButton[] getButtons(){
+    return boatButtons;
+  }
+  
 
 }
