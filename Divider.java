f2212b0a335bce53e4190037642c3c76055fddf7
w1sely <chen.wisley@gmail.com>
2017-01-18 22:53:36 -0500

    Update Divider.java

--- Divider.java
+++ Divider.java
@@ -5,12 +5,22 @@ import java.awt.event.*;
 public class Divider extends JPanel {
   
   //Creating 6 JButtons
-  JButton[] jb = new JButton[6];
+  JButton[] boatButtons = new JButton[6];
   
   //Declaring final String array size
   public final String[] size = new String[1];
+  
+  //Declares left filed instance
+  LeftField leftField;
 
-  //Divider 
+  //Creates an instance of left field in divider
+   public Divider(LeftField fieldLeft)
+  {
+    leftField = fieldLeft;
+    init();
+  }
+   
+  //Initializes Divider 
   public Divider()
   {
     init();
@@ -36,7 +46,7 @@ public class Divider extends JPanel {
     add(divider, BorderLayout.NORTH); //Adding divider with border layout 
     
     //Adding title of Label
-    JLabel title = new JLabel("                                                                                                 Ships");
+    JLabel title = new JLabel("                                                                    Ships");
     add(title, BorderLayout.NORTH); //Adding title with border layout
     
     //Adding middle panel
@@ -44,31 +54,103 @@ public class Divider extends JPanel {
     middlePanel.setLayout(new GridLayout(6 , 1)); //Setting layout of middlePanel
     
     //ActionListener buttonListen = new ActionListener();
+    
+    
     //For loop to create buttons and adding an action listener to each of them
     for (int i = 5; i >= 0; i--){
-      jb[i] = new JButton(); //Creating new button
-      jb[i].setName((i+1)+""); //Setting name
-      jb[i].setPreferredSize(new Dimension(10,10)); //Setting size of button                               
+      boatButtons[i] = new JButton(); //Creating new button
+      boatButtons[i].setName((i+1)+""); //Setting name
+      boatButtons[i].setPreferredSize(new Dimension(10,10)); //Setting size of button                               
       //jb[i][j].addActionListener(buttonListen);
-      middlePanel.add(jb[i]);
+      middlePanel.add(boatButtons[i]);
       
       //Adding action listener to each button
-      jb[i].addActionListener(new ActionListener(){
+      boatButtons[i].addActionListener(new ActionListener(){
                 
         public void actionPerformed(ActionEvent event) {
           
-          size[0] = ((JButton)event.getSource()).getName();          
+          size[0] = ((JButton)event.getSource()).getName();
+          
         }
       });     
     }
     
     //Setting each button with appropriate image
-    jb[5].setIcon(ac);
-    jb[4].setIcon(destroyer);
-    jb[3].setIcon(sub);
-    jb[2].setIcon(patrol);
-    jb[1].setIcon(raft);
-    jb[0].setIcon(done);
+    boatButtons[5].setIcon(ac);
+    boatButtons[4].setIcon(destroyer);
+    boatButtons[3].setIcon(sub);
+    boatButtons[2].setIcon(patrol);
+    boatButtons[1].setIcon(raft);
+    boatButtons[0].setIcon(done);
+    
+     //Add action listener for all boat buttons       
+  boatButtons[1].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          boatButtons[1].setEnabled(false);
+          
+          
+        }
+      });
+  
+    
+  boatButtons[2].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          boatButtons[2].setEnabled(false);
+          
+          
+        }
+      });
+  
+    
+  boatButtons[3].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          boatButtons[3].setEnabled(false);
+          
+          
+        }
+      });
+  
+    
+  boatButtons[4].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          boatButtons[4].setEnabled(false);
+          
+          
+        }
+      });
+    
+  boatButtons[5].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          boatButtons[5].setEnabled(false);
+          
+          
+        }
+      }); //end of action listeners for all boat buttons
+    
+   //Add action listener for done button 
+  boatButtons[0].addActionListener(new ActionListener(){
+             
+        public void actionPerformed(ActionEvent event) {
+          
+          for (int i = 5; i >= 0; i--){
+          boatButtons[i].setEnabled(false);
+          }     
+
+          
+        }
+      });//end of done button action listener
+    
+  
    
     add(middlePanel, BorderLayout.CENTER);  
   }
@@ -79,6 +161,6 @@ public class Divider extends JPanel {
     * @return jb  JButton
     */
   public JButton[] getButtons(){
-    return jb;
+    return boatButtons;
   }
 }
