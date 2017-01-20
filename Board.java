61e93099b75644a7f45b27902e64a0a12ae1c839
w1sely <chen.wisley@gmail.com>
2017-01-19 22:16:47 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,6 +1,14 @@
+/* Conflicts Boats
+ * You play against a computer, and try to hit their ships
+ * Wisley Chen, Paul Jang, Leon Wang
+ * January 20, 2017
+ */
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import javax.imageio.*;
+import java.io.*;
 
 public class Board {
   
@@ -22,174 +30,94 @@ public class Board {
     //Creating divider and leftfield objects
     final Divider divider = new Divider();
     final LeftField leftField = new LeftField(divider);
-    final RightField rightField = new RightField(divider);
+    final RightField rightField = new RightField(leftField);
     
-    final JPanel credits = new JPanel ();
+    final JPanel credits = new JPanel ();    
+    
+    //Sets up instruction panel image
     final JPanel instructions = new JPanel ();
+    ImageIcon instructionsImage = new ImageIcon ("instructionsImage.jpg");
+    JLabel imageLabel2 = new JLabel(instructionsImage);
+    instructions.add(imageLabel2);
     
+    //Sets up main menu background
+    final JPanel mainMenu = new JPanel();    
+    frame.add(mainMenu);
+
     
     //Setting up game window 
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
+    frame.setVisible(true);
     frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
     
-    final JPanel mainMenu = new JPanel();    
-    frame.add(mainMenu);
-    
+   
+//------------------ACTION LISTENER FOR MAIN MENU BUTTONS------------------
     ActionListener al = new ActionListener(){
       public void actionPerformed(ActionEvent e){
+        
+        //If the button is pressed
         if(!(e.getSource() instanceof JButton)){
           return; 
         }
         
         JButton button = (JButton) e.getSource();
         
-        
+        //If start button is pressed
         if(button.getText().equalsIgnoreCase("Start")){
-          // Button got pressed 
-          //Adding classes 
           frame.remove(mainMenu);
           frame.add(leftField);
           frame.add(divider);
           frame.add(rightField);
-          frame.validate();
-          
-        }else if(button.getText().equalsIgnoreCase("quit")){
-          System.exit(0); 
-        }
-      }
-      
-    };
-    
-    ActionListener alCredits = new ActionListener(){
-      public void actionPerformed(ActionEvent d){
-        if(!(d.getSource() instanceof JButton)){
-          return; 
-        }
+          frame.validate(); 
+        }//end of start 
         
-        JButton button = (JButton) d.getSource();
-        
-        
-        if(button.getText().equalsIgnoreCase("Credits")){
-          //Button got pressed 
-          //Adding classes 
+        //If credits button is pressed        
+        else if(button.getText().equalsIgnoreCase("Credits")){
           frame.remove(mainMenu);
           frame.add(credits);
-          frame.setVisible(false);
-          frame.setVisible(true);
-          frame.validate();
-          
-        }
-        else if(button.getText().equalsIgnoreCase("quit")){
-          System.exit(0); 
-        }
-      }
-    };
-    
-    ActionListener alInstruction = new ActionListener(){
-      public void actionPerformed(ActionEvent f){
-        if(!(f.getSource() instanceof JButton)){
-          return; 
-        }
+          frame.validate();         
+        }//end of credits
         
-        JButton button = (JButton) f.getSource();
-        
-        
-        if(button.getText().equalsIgnoreCase("Instructions")){
-          //Button got pressed 
-          //Adding classes 
+        //If instructions button is pressed
+        else if(button.getText().equalsIgnoreCase("Instructions")){
           frame.remove(mainMenu);
           frame.add(instructions);
-          frame.validate();
-          frame.setVisible(false);
-          frame.setVisible(true);
-          
-        }
-        else if(button.getText().equalsIgnoreCase("quit")){
+          frame.validate();       
+        }//end of instructions
+        
+        //If quit button is pressed
+        else if(button.getText().equalsIgnoreCase("Quit")){
           System.exit(0); 
-        }
-      }
-    };
+        }//end of quit
+        
+      }   
+    };//end of action listener
     
-    ActionListener backInstructions = new ActionListener(){
-      public void actionPerformed(ActionEvent g){
-        if(!(g.getSource() instanceof JButton)){
-          return; 
-        }
-        
-        JButton button = (JButton) g.getSource();
-        
-        
-        if(button.getText().equalsIgnoreCase("Back")){
-          //Button got pressed 
-          //Adding classes         
-          frame.remove(instructions);
-          frame.add(mainMenu);
-          frame.validate();
-          frame.setVisible(false);
-          frame.setVisible(true);
-        }
-        else if(button.getText().equalsIgnoreCase("quit")){
-          System.exit(0); 
-        }
-      }
-    };
-    
-    ActionListener backCredits = new ActionListener(){
-      public void actionPerformed(ActionEvent h){
-        if(!(h.getSource() instanceof JButton)){
-          return; 
-        }
-        
-        JButton button = (JButton) h.getSource();
-        
-        
-        if(button.getText().equalsIgnoreCase("Back")){
-          //Button got pressed 
-          //Adding classes         
-          frame.remove(credits);
-          frame.add(mainMenu);
-          frame.validate();
-          frame.setVisible(false);
-          frame.setVisible(true);
-          
-        }
-        else if(button.getText().equalsIgnoreCase("quit")){
-          System.exit(0); 
-        }
-      }
-    };
-    
-    
+    //Creation of main menu buttons
     JButton start = new JButton ("Start");
     start.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
     start.addActionListener(al);
     mainMenu.add(start);
-     
+    
     JButton instructionButton = new JButton("Instructions");
     instructionButton.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
-    instructionButton.addActionListener(alInstruction);
+    instructionButton.addActionListener(al);
     mainMenu.add(instructionButton);
     
     JButton creditsButton= new JButton ("Credits");
     creditsButton.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
-    creditsButton.addActionListener(alCredits);
+    creditsButton.addActionListener(al);
     mainMenu.add(creditsButton);
-    
-    JButton back1 = new JButton("Back");
-    JButton back2 = new JButton("Back");
-    back1.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
-    back2.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
-    back1.addActionListener(backInstructions);
-    back2.addActionListener(backCredits);
-    instructions.add(back1);
     
     JButton quit = new JButton("Quit");
     quit.setFont(new Font(start.getFont().getName(), Font.PLAIN, 50));
     quit.addActionListener(al);
     mainMenu.add(quit);
     
+//------------------CREDIT SCREEN CREATION------------------
+    
+    //Adds all the text 
     JLabel jlabel1 = new JLabel("CREDITS");
     JLabel jlabel2 = new JLabel("Images:");
     JLabel jlabel3 = new JLabel("Developers:");
@@ -200,11 +128,12 @@ public class Board {
     JLabel patrolboat = new JLabel("https://it.pinterest.com/pin/288934132325552202/");
     JLabel raft = new JLabel("http://www.supercoloring.com/silhouettes/rafting");
     JLabel waterLink = new JLabel ("https://www.johnweiss.ca/single-post/2016/10/08/The-Water-Element");
-    JLabel x = new JLabel ("http://www.freeiconspng.com/png-images/x-png");
+    JLabel background = new JLabel ("http://wonderfulengineering.com/amazing-hd-navy-wallpapers-and-backgrounds-for-free-download/");
     JLabel name1 = new JLabel("Wisley Chen");
     JLabel name2 = new JLabel("Paul Jang");
     JLabel name3 = new JLabel("Leon Wang");
     
+    //Sets font and size for all the text
     jlabel1.setFont(new Font("Verdana",1,50));
     jlabel2.setFont(new Font("Verdana",1,25));
     jlabel3.setFont(new Font("Verdana",1,25));
@@ -214,12 +143,12 @@ public class Board {
     patrolboat.setFont(new Font("Verdana",1,20));
     raft.setFont(new Font("Verdana",1,20));
     waterLink.setFont(new Font("Verdana",1,20));
-    x.setFont(new Font("Verdana",1,20));
+    background.setFont(new Font("Verdana",1,20));
     name1.setFont(new Font("Verdana",1,20));
     name2.setFont(new Font("Verdana",1,20));
     name3.setFont(new Font("Verdana",1,20));
     
-    
+    //Adds all of text to the panel
     credits.add(jlabel1);
     credits.add(jlabel2);
     credits.add(destroyer);
@@ -228,20 +157,21 @@ public class Board {
     credits.add(patrolboat);
     credits.add(raft);
     credits.add(waterLink);
-    credits.add(x);
+    credits.add(background);
     credits.add(space);
     credits.add(jlabel3);
     credits.add(name1);
     credits.add(name2);
     credits.add(name3);
-    credits.add(Box.createRigidArea(new Dimension(0,400)));
-    credits.add(back2);
+    
+    //Sets some text to different colours
     name1.setForeground (Color.red);
     name2.setForeground (Color.green);
     name3.setForeground (Color.magenta);
-    frame.setVisible(true);
     
+    //Sets credits page as a box layout
     credits.setLayout(new BoxLayout(credits, BoxLayout.Y_AXIS));
-  
-  }
-}
+    
+       
+  }//end of main
+}//end of board
